9ddd367f71797d4422934b9453146a573bed9d36
sj-prakash <js0077@srmist.edu.in>
2023-08-09 21:59:04 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import './App.css';
+import MovieCard from './MovieCard';
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=cdf34a35';
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [searchTerm, setSearchTerm] = useState([]);
+	const searchMovies = async (title) => {
+		const response = await fetch(`${API_URL}&s=${title}`);
+		const data = await response.json();
+		setMovies(data.Search);
+	}
+	useEffect(() => {
+		searchMovies('Batman');
+	}, []);
+	return (
+		<div className="app">
+			<h1>Cine Sphere</h1>
+
+			<div className="search">
+				<input
+					placeholder="Search for Movies"
+					value={searchTerm}
+					onChange={(e) => { setSearchTerm(e.target.value) }}
+				/>
+
+        
+				<img
+					src=
+"https://media.geeksforgeeks.org/wp-content/uploads/20230626112934/search.png"
+					alt="search icon"
+					onClick={() => searchMovies(searchTerm)}
+				/>
+			</div>
+
+
+			{
+				movies?.length > 0
+					? (<div className="container">
+						{movies.map((movie) => (
+							<MovieCard movie={movie} />
+						))}
+					</div>) : (
+						<div className="empty">
+							<h2>No Movies found</h2>
+						</div>
+					)
+			}
+		</div>
+	);
+}
+export default App;
